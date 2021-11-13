e51beca72d7a1df9fc3cf6ae625de0ad04ec773b
Luis0381 <luiseugeniomr@gmail.com>
2021-11-13 18:15:46 -0300

    Implementacion de interfaces IControladorAMAlumno y IControladorPrincipal

---
+++ src/interfaces/IControladorPrincipal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * @author Luis Medina Raed
+ */
+public interface IControladorPrincipal {
+
+    public static final String CONFIRMACION = "¿Desea terminar la sesión?";
+    public static final String TITULO = "Repositorios";
+
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Autores
+     *
+     * @param evt evento
+     */
+    public void btnAutoresClic(ActionEvent evt);
+
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Salir
+     *
+     * @param evt evento
+     */
+    public void btnSalirClic(ActionEvent evt);
+
+}
